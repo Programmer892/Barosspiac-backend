6919ec42545e66287d574cab887f344019f69156
diak <diak@mesterlevel.lo>
2026-04-22 12:22:46 +0200

    config2

--- config/db.js
+++ config/db.js
@@ -4,10 +4,11 @@ dotenv.config()
 
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "barosspiac3",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PSW,
+    database: process.env.DB_NAME,
     connectionLimit: 10,
     queueLimit: 0
 
